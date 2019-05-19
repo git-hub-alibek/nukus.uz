909b75720a8e90683fc63d8d66c6c94cca5364f0
git-hub-alibek <taza.nik76@gmail.com>
2019-05-19 13:41:03 +0500

    213

---
+++ application/language/russian/custom_lang.php
@@ -0,0 +1,141 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+//team
+$lang['title_name'] = 'Анализатор сообщений | Специально для Wincode';
+
+
+
+
+
+// menu
+$lang['menu_home'] = 'Главная';
+$lang['menu_about'] = 'О нас';
+$lang['menu_help'] = 'О нас';
+$lang['menu_feedback'] = 'Обратная связь';
+$lang['menu_codes'] = 'Коды';
+$lang['menu_gallery'] = 'Галерея';
+$lang['menu_service'] = 'Сервисы';
+$lang['menu_team'] = 'Наша Команда';
+$lang['menu_contact'] = 'Контакты';
+$lang['menu_rus'] = 'РУ';
+$lang['menu_en'] = 'EN';
+
+
+ 
+
+
+
+//team
+$lang['team_title'] = 'Наша команда';
+$lang['Kadirov'] = 'Азизбек Кадыров';
+$lang['Kadirov_info'] = 'Лидер &amp; Разработчик';
+$lang['Embergenov'] = 'Алибек Ембергенов';
+$lang['Embergenov_info'] = 'Front-end разработчик';
+$lang['Nurakhimov'] = 'Алланияз Нурахимов';
+$lang['Nurakhimov_info'] = 'Разработчик';
+$lang['Turimbetov'] = 'Бахыт Турымбетов';
+$lang['Turimbetov_info'] = 'Разработчик';
+
+//contact
+$lang['contact_us_title'] = 'Наш адрес';
+// views/feedback.php
+$lang['feedback_heading'] = 'Связаться с нами';
+$lang['feedback_name'] = 'Имя';
+$lang['feedback_placeholder_name'] = 'Ваше имя';
+$lang['feedback_email'] = 'Почта';
+$lang['feedback_placeholder_email'] = 'Email';
+$lang['feedback_theme'] = 'Тема';
+$lang['feedback_placeholder_theme'] = 'Тема';
+$lang['feedback_text'] = 'Текст';
+$lang['feedback_placeholder_text'] = 'Текст';
+$lang['feedback_protection'] = 'Защитный код';
+$lang['feedback_placeholder_protection'] = 'Защитный код';
+$lang['feedback_send'] = 'Отправить';
+
+$lang['designed'] = "Разработан командой <a href='#'>TATUNF</a>";
+
+//bd
+$lang['menu_message'] = 'Сообщения';
+$lang['menu_users'] = 'Пользователи';
+$lang['menu_groups'] = 'Группы';
+$lang['menu_static_all'] = 'Общая Статистика';
+$lang['menu_static'] = 'Статистика Периодная';
+$lang['menu_logout'] = 'Выход';
+$lang['menu_login'] = 'Вход';
+$lang['admin'] = 'Админ';
+
+//auth index.php
+$lang['table_name']= 'Имя';
+$lang['table_surname']= 'Фамилия';
+$lang['table_phone']= 'Телефон';
+$lang['table_groups']= 'Группы';
+$lang['table_status']= 'Статус';
+$lang['table_actions']= 'Операции';
+$lang['table_users']= 'Пользователи';
+
+
+//team
+$lang['t_azizbek'] = 'Кадыров Азизбек';
+$lang['t_text_azizbek'] = 'Лидер & Back-end Разработчик';
+$lang['t_alibek'] = 'Ембергенов Алибек';
+$lang['t_text_alibek'] = 'Front-end Разработчик';
+$lang['t_allaniyaz'] = 'Нурахимов Алланияз';
+$lang['t_text_allaniyaz'] = 'Back-end Разработчик';
+$lang['t_baxit'] = 'Турымбетов Бахыт';
+$lang['t_text_baxit'] = 'Back-end Разработчик';
+
+//copyright
+$lang['copyright'] = '© 2018 Open Data Ташкент . Все Права Защищены | Design by TATU NF Team';
+
+
+$lang['panorama_city'] = 'Панорама Города';
+$lang['new_events']= 'Новости и Событие';
+$lang['address']= 'Адрес';
+$lang['pages']= 'Страницы';
+$lang['follow']= 'Подписаться';
+$lang['smart_uslugi']= 'Умные предложения';
+
+$lang['smart_city'] = 'Умный Город';
+$lang['smart_idea'] = 'Умная Идея';
+$lang['smart_chat'] = 'Умный Чат';
+$lang['smart_bot'] = 'Умный Бот';
+$lang['temperature'] = 'Температура';
+$lang['smart_search'] = 'Умный Поиск';
+
+
+//temperatura
+$lang['city'] = 'Город';
+$lang['tashkent'] = 'Ташкент';
+$lang['gradus'] = 'Градус  °C';
+$lang['nukus'] = 'Нукус';
+$lang['urgench'] = 'Ургенч';
+$lang['bukhara'] = 'Бухара';
+$lang['navoiy'] = 'Навоий';
+$lang['samarkand'] = 'Самарканд';
+$lang['jizzax'] = 'Жиззах';
+$lang['gulistan'] = 'Гулистан';
+$lang['qarshi'] = 'Карши';
+$lang['termez'] = 'Термиз';
+$lang['fergana'] = 'Фергана';
+$lang['namangan'] = 'Наманган';
+$lang['andijan'] = 'Андижан';
+$lang['nurafshon'] = 'Нурафшан';
+
+
+$lang['button_send'] = 'Отправить';
+// search
+$lang['smart_assistent'] = 'Умный Ассистент';
+$lang['search_sistem'] = 'Умный Поиск';
+$lang['search_find'] = 'Найти...';
+$lang['inha'] = 'Университет Инха Ташкент';
+
+//about
+$lang['about_us'] = 'Использование этого продукта очень просто!';
+$lang['about_text'] = 'Все, что вам нужно - это мобильное приложение или мессенджер Telegram. Теперь вы можете обратиться за помощью и Ассистент поможет вам';
+$lang['about_title'] = 'Найдите ответы на свои вопросы';
+$lang['our_team'] = 'Наша Команда';
+
+
+
