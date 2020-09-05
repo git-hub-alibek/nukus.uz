f5e97f257d680d12709bbc7e1905da101d667de1
git-hub-alibek <taza.nik76@gmail.com>
2020-09-05 13:01:13 +0500

    123

---
+++ application/views/send.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo lang('menu_vacancy');?></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="/assets/images/favicon.png">
+        
+        <!-- SweetAlert -->
+        <link rel="stylesheet" href="/assets/salert/sweetalert2.min.css">
+        <script type="text/javascript" src="/assets/salert/sweetalert2.all.min.js"></script>
+</head>
+<body>
+
+
+
+	<script type="text/javascript">
+		swal('<?php echo lang('thanks'); ?>', '', 'success');
+	</script>
+
+	
+
+</body>
+</html>
+
